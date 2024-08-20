fde04607125c9d873ac846f681543b5634dc3088
The-Gryphon-Project <154812768+The-Gryphon-Project@users.noreply.github.com>
2024-08-20 22:59:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,8 @@ context_length = 30
 api_key = os.getenv("OPENAI_API_KEY")
 organization = os.getenv("OPENAI_ORGANISATION")
 
-st.text(api_key, organization)
+st.text(api_key)
+st.text(organization)
 
 # client = OpenAI(
 #     api_key=api_key,
